d7599dfdb73efddfa8c5d4b7ca2a5bce26929b7d
storitellah <80640856+storitellah@users.noreply.github.com>
2026-05-27 13:18:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  * the old cache and forces a fresh fetch of the HTML.
  */
 
-const CACHE_VERSION = 'invoyou-v9';
+const CACHE_VERSION = 'invoyou-v10';
 const APP_SHELL = [
   '/',
   '/index.html',
